ef8b1f0d7162457f70f8c320cf137232bfea1fa6
fikri-enha <fikrihaikal.nh@gmail.com>
2022-05-08 18:43:18 +0700

    solve error in sampling.sh

--- sampling.sh
+++ sampling.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-wget -O weather_data.xlsx https://github.com/labusiam/dataset/raw/main/weather_data.xlsx |
-	in2csv --sheet weather_2014 weather_data.xlsx > weather_2014.csv |
-	in2csv --sheet weather_2015 weather_data.xlsx > weather_2015.csv |
-	csvstack weather_2014.csv weather_2015.csv > weather.csv | 
-	rm weather_data.xlsx |
-	csvlook weather.csv |
-	sample -r 30% >> sample_weather.csv 
+wget -O weather_data.xlsx https://github.com/labusiam/dataset/raw/main/weather_data.xlsx
+in2csv weather_data.xlsx --sheet "weather_2014" > weather_2014.csv 
+in2csv weather_data.xlsx --sheet "weather_2015" > weather_2015.csv
+csvstack weather_2014.csv weather_2015.csv >> weather.csv
+rm weather_data.xlsx 
+csvlook weather.csv | sample -r 30% >> sample_weather.csv 
